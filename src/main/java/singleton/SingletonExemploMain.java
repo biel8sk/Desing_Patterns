2d1ce77364c49165cb9ba54a473815431b77982b
Gabriel Pires <dasaybarreto724@gmail.com>
2025-09-03 18:37:08 -0300

    executando tudo em um lugar só

--- src/main/java/singleton/SingletonExemploMain.java
+++ src/main/java/singleton/SingletonExemploMain.java
@@ -4,13 +4,23 @@
  */
 package singleton;
 
+import singleton.logger.comPattern.ApplicationWithSingleton;
+import singleton.logger.comPattern.ConcurrentLoggerWithSingleton;
 import singleton.logger.semPattern.ApplicationWithoutSingleton;
+import singleton.logger.semPattern.ConcurrentLoggerWithoutSingleton;
+
 
 /**
  *
  * @author olgac
  */
 public class SingletonExemploMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationWithoutSingleton.main(args);
+        ApplicationWithSingleton.main(args);
+        System.out.println("\n");
+        System.out.println("Testando concorrencia");
+        ConcurrentLoggerWithoutSingleton.main(args);
+        ConcurrentLoggerWithSingleton.main(args);
     }
 }
