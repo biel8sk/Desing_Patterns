b5a840db8ea4c525984c596c1fd7db472f1ff216
Gabriel Pires <dasaybarreto724@gmail.com>
2025-10-11 21:58:29 -0300

    target e configurações

--- src/main/java/singleton/logger/semPattern/ConcurrentLoggerWithoutSingleton.java
+++ src/main/java/singleton/logger/semPattern/ConcurrentLoggerWithoutSingleton.java
@@ -12,7 +12,7 @@ public class ConcurrentLoggerWithoutSingleton {
     public static void main(String[] args ) throws InterruptedException{
         System.out.println("Testando concorrência SEM Singleton");
 
-        Runnable task = () -> {
+        Runnable task1 = () -> {
             LoggerManeger logger = new LoggerManeger();
             for (int i = 0; i < 5; i++){
                 // printando o logger a thread atual
@@ -24,11 +24,26 @@ public class ConcurrentLoggerWithoutSingleton {
                     System.out.println(e.toString());
                 }
             }
+            logger.log("Termminando tarefa 1 \n");
+        };
+        Runnable task2 = () -> {
+            LoggerManeger logger = new LoggerManeger();
+            for (int i = 0; i < 5; i++){
+                // printando o logger a thread atual
+                logger.log("Mensagem na thread :" + Thread.currentThread().getName() + " - " + i);
+                
+                try {
+                    Thread.sleep(85); // Pequena pausa para simular processamento
+                } catch (InterruptedException e) {
+                    System.out.println(e.toString());
+                }
+            }
+            logger.log("Termminando tarefa 2 \n");
         };
         
         // criando threads 
-        Thread th1 = new Thread(task, "Th1");
-        Thread th2 = new Thread(task, "TH2");
+        Thread th1 = new Thread(task1, "Th1");
+        Thread th2 = new Thread(task2, "TH2");
         
         th1.start();
         th2.start();
